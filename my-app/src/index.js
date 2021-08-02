b89fb40dcb99fbe3da165abc900a2b294d555c9f
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-08-01 23:31:31 -0500

    jsx

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+    <div className="form">
+        <div className="form-toggle"></div>
+        <div className="form-panel one">
+            <div className="form-header">
+                <h1>Account Login</h1>
+            </div>
+            <div className="form-content">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text" name="username" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="password" name="password" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <label className="form-remember">
+                            <input type="checkbox"/>Remember Me
+                        </label><a className="form-recovery" href="#">Forgot Password?</a>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit">Log In</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div className="form-panel two">
+            <div className="form-header">
+                <h1>Register Account</h1>
+            </div>
+            <div className="form-content">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text" name="username" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="password" name="password" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cpassword">Confirm Password</label>
+                        <input id="cpassword" type="password" name="cpassword" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email Address</label>
+                        <input id="email" type="email" name="email" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    );
 };
 
 
