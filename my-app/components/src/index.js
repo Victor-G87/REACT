3af15588ce7aed9cfea2cc6b69e02e5cfea7d7c4
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-08-04 01:37:34 -0500

    multi props

--- my-app/components/src/index.js
+++ my-app/components/src/index.js
@@ -6,10 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
       <div className="ui container comments">
-          <CommentDetail author='Sam' />
-          <CommentDetail author='Alex' />
-          <CommentDetail author='Jane' />
-          <CommentDetail author='Bob' />
+          <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:14PM'
+          userComment='is that it...'
+          avatar={faker.image.avatar()}
+          />
+          <CommentDetail
+           author='Alex'
+           timeAgo='Today at 7:42PM'
+           userComment='ok'
+           avatar={faker.image.avatar()}
+          />
+          <CommentDetail
+           author='Jane'
+           timeAgo='Today at 9:34PM'
+           userComment='this is amazing'
+           avatar={faker.image.avatar()}
+          />
       </div>
     );
 };
