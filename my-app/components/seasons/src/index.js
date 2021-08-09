3b1bd2bd8fca6add6069dc7f1012e17728cf1e1f
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-08-09 02:26:31 -0500

    refactor

--- my-app/components/seasons/src/index.js
+++ my-app/components/seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
 
 
 // const App = () => {
@@ -29,7 +30,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: { this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <div>Loading!</div>
