8d82cf6a2d03a5d15375f121b3bb03aa2711535f
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-08-18 15:41:07 -0500

    weather update pc

---
+++ weather/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
+//     return <div>Latitude: </div>
+// };
+
+
+class App extends React.Component {
+
+    state = { late: null, errorMessage: ''};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message})
+        );
+    }
+
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: { this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div>Loading!</div>
+
+    }
+}
+
+
+
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
