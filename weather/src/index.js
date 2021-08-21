a6f71345007ff46b24f840786d83bf5971ff85ff
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-08-20 21:01:54 -0500

    loading

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+
 
 
 // const App = () => {
@@ -33,7 +36,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>
+        return <Spinner />
 
     }
 }
